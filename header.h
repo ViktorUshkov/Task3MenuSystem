c5cc1636d89c213445178f7506c4cf50d5c84116
ViktorUshkov <63870567+ViktorUshkov@users.noreply.github.com>
2020-05-28 03:00:28 +0300

    Add files via upload

---
+++ header.h
@@ -0,0 +1,30 @@
+#pragma once
+#include <Windows.h>
+#include <stdio.h>
+
+typedef struct
+{
+	char* name;
+	POINT position[2];
+	COLORREF color, highlighted_color;
+	int win_link;
+}button_t;
+
+typedef struct
+{
+	int but_count, level, show;
+	button_t* buttons;
+}window_t;
+
+typedef struct 
+{
+	int win_count, current_win, current_but;
+	window_t* windows;
+}menu_t;
+
+char* Word(FILE* file);
+int Number(FILE* file);
+menu_t* Extract();
+
+void Check(menu_t* menu, int key);
+void Draw(HDC hdc, menu_t* menu, int key);
---
+++ main.c
@@ -0,0 +1,110 @@
+﻿#include <windows.h>
+#include "header.h"
+
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+char szProgName[] = "menu_system";
+
+int key_condition = 0;
+menu_t* menu;
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdLine, int nCmdShow)
+{
+	menu = Extract();
+
+	HWND hWnd;
+	MSG lpMsg;
+	WNDCLASS w;
+
+	w.lpszClassName = szProgName;
+	w.hInstance = hInstance;
+	w.lpfnWndProc = WndProc;
+	w.hCursor = LoadCursor(NULL, IDC_ARROW);
+	w.hIcon = 0;
+	w.lpszMenuName = 0;
+	w.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
+	w.cbClsExtra = 0;
+	w.style = CS_HREDRAW | CS_VREDRAW;
+	w.cbWndExtra = 0;
+
+	if (!RegisterClass(&w))
+		return 0;
+
+
+	hWnd = CreateWindow(szProgName, "Just Menu", WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT,
+		CW_USEDEFAULT, CW_USEDEFAULT, (HWND)NULL, (HMENU)NULL, (HINSTANCE)hInstance, (HINSTANCE)NULL);
+
+	ShowWindow(hWnd, SW_SHOWMAXIMIZED);
+	UpdateWindow(hWnd);
+
+	while (GetMessage(&lpMsg, hWnd, 0, 0)) {
+		TranslateMessage(&lpMsg);
+		DispatchMessage(&lpMsg);
+	}
+	return (int)lpMsg.wParam;
+}
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT messg, WPARAM wParam, LPARAM lParam)
+{
+	HDC hdc;
+	PAINTSTRUCT ps;
+	RECT rect;
+
+	GetClientRect(hWnd, &rect);
+
+	switch (messg)
+	{
+	case WM_KEYDOWN:
+		switch (wParam)
+		{
+		case VK_DOWN:
+		{
+			key_condition = 1;
+			InvalidateRect(hWnd, &rect, TRUE);
+			UpdateWindow(hWnd);
+			break;
+		}
+		case VK_UP:
+		{
+			key_condition = 2;
+			InvalidateRect(hWnd, &rect, TRUE);
+			UpdateWindow(hWnd);
+			break;
+		}
+		case VK_RIGHT:
+		{
+			key_condition = 3;
+			InvalidateRect(hWnd, &rect, TRUE);
+			UpdateWindow(hWnd);
+			break;
+		}
+		case VK_LEFT:
+		{
+			key_condition = 4;
+			InvalidateRect(hWnd, &rect, TRUE);
+			UpdateWindow(hWnd);
+			break;
+		}
+		default:
+			break;
+		}
+
+	case WM_KEYUP:
+		if (wParam == 27) ExitProcess(0);
+		break;
+
+	case WM_PAINT:
+	{
+		hdc = BeginPaint(hWnd, &ps);
+		Draw(hdc, menu, key_condition);
+		EndPaint(hWnd, &ps);
+		break;
+	}
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+
+	default:
+		break;
+	}
+	return DefWindowProc(hWnd, messg, wParam, lParam);
+}
